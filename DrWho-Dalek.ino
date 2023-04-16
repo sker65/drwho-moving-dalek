9ef3e6c5a1400e50800dc06f920da71d59452744
Stefan Rinke <sker65@gmail.com>
2023-04-16 17:01:20 +0200

    - 2nd version

--- DrWho-Dalek.ino
+++ DrWho-Dalek.ino
@@ -1,70 +1,77 @@
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+Servo dalek;  // create servo object to control a servo
 // a maximum of eight servo objects can be created
-
-int pos = 90;    // variable to store the servo position
-
-boolean moving = false;
 
 #define SOL_DALEK 4
 #define OPTO 5
+#define SWITCH_LED 13
+#define ZERO_POS 90
+#define MAX_POS 170
+#define MIN_POS 0
+#define STEP 2
+#define ZEROPOS_DELTA 4
+
+int pos = ZERO_POS;    // variable to store the servo position
+
+boolean moving = false;
 
 void setup()
 {
-  myservo.attach(3);  // attaches the servo on pin 3 to the servo object
+  dalek.attach(3);  // attaches the servo on pin 9 to the servo object
   pinMode(SOL_DALEK,INPUT);
   pinMode(OPTO, OUTPUT);
-  pinMode(13,OUTPUT);
+  pinMode(SWITCH_LED,OUTPUT);
   digitalWrite( OPTO, HIGH);
-  digitalWrite( 13, LOW);
-  myservo.write(90);
+  dalek.write(90);
   //Serial.begin(9600);
 }
 
-#define MAX_POS 170
-#define STEP 2
 
 boolean forward = true;
 
 void loop()
 {
-//    myservo.write(90);
+//    dalek.write(90);
 
   boolean moving = (digitalRead(SOL_DALEK) == LOW);
-  digitalWrite(13, moving);
+  //digitalWrite(13, moving);
 
-  if( pos >= 92 || pos <= 88 ) {
-    digitalWrite(OPTO,LOW);
-  } else {
+  if( pos >= ZERO_POS+ZEROPOS_DELTA || pos <= ZERO_POS-ZEROPOS_DELTA ) {
     digitalWrite(OPTO,HIGH);
+    digitalWrite( SWITCH_LED, HIGH);
+  } else {
+    digitalWrite(OPTO,LOW);
+    digitalWrite( SWITCH_LED, LOW);
   }
 
-  if( moving || pos >= 92 || pos <= 88  ) {
+  if( moving || pos >= ZERO_POS+ZEROPOS_DELTA || pos <= ZERO_POS-ZEROPOS_DELTA  ) {
 
     if( pos < MAX_POS && forward ) {
       pos += STEP;
     }
 
-    if( pos >= 0 && !forward ) {
+    if( pos >= MIN_POS && !forward ) {
       pos -= STEP;
     }
 
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    dalek.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
     
     
     if( pos >= MAX_POS ) {
       forward = false;
     }
-    if( pos <= 0 ) {
+    if( pos <= MIN_POS ) {
       forward = true;
     }
     
     //Serial.println(pos);
 
   } else {
-    myservo.write(90);  
+    dalek.write(ZERO_POS);  
   }
   
 }
+
+
